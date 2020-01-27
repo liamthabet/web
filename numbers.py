f2aba99572ade8f08015db8c2001d2adfdf08878
liamthabet <60346960+liamthabet@users.noreply.github.com>
2020-01-27 12:08:35 +0000

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,17 @@
+#get num of usernames to be generated ny the user
+
+#create list of endings
+
+username= int(input('how many usernames do you want to be generated'))
+
+
+
+#repeat number of times user entered
+
+
+
+
+
+
+
+
